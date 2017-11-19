6ad95a80a75c1727bf2ce7580ccdee4ec7f05d8c
Dulce <dulcerubio2018@gmail.com>
2017-11-19 01:06:39 +0000

    array

---
+++ arrays.js
@@ -0,0 +1,51 @@
+var fourLetterCountries = [
+    
+    "Chad",
+    "Cuba",
+    "Greenland",
+    "Iraq",
+    "Mali",
+    "Oman",
+    "India"
+    ]
+    
+    
+ fourLetterCountries.push("Fiji")
+ console.log(fourLetterCountries)
+ 
+ fourLetterCountries.splice(4,0, "Iran")
+ console.log(fourLetterCountries)
+ 
+ fourLetterCountries.splice(6,1)
+console.log(fourLetterCountries)
+ 
+ fourLetterCountries.reverse()
+console.log(fourLetterCountries)
+
+ fourLetterCountries.splice(2,1, "Togo")
+console.log(fourLetterCountries)
+
+fourLetterCountries.push("Laos")
+ console.log(fourLetterCountries)
+
+fourLetterCountries.reverse()
+console.log(fourLetterCountries)
+
+fourLetterCountries.unshift("Peru")
+console.log(fourLetterCountries)
+
+
+
+
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
